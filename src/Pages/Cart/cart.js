16fb447ade221d26784e4232bff41519f603cc06
chibuike-vincent <chibuikepatrick2@gmail.com>
2022-02-18 09:18:28 +0100

    completed card payment

--- src/Pages/Cart/cart.js
+++ src/Pages/Cart/cart.js
@@ -1,28 +1,80 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import "./cart.css"
+import { makeStyles } from '@mui/styles';
 import { Grid, Box, Dialog, DialogTitle, DialogContent, IconButton } from '@mui/material';
+import { TextField, FormControl, FormLabel, RadioGroup, FormControlLabel, Radio } from "@mui/material";
+import { MuiPickersUtilsProvider, KeyboardDatePicker, DateTimePicker } from '@material-ui/pickers';
+import MomentUtils from '@date-io/moment';
 import { FiX } from "react-icons/fi";
 import drugsone from '../../images/drug-one.jpg'
 import Badge from '@mui/material/Badge';
 import { FaShoppingCart } from "react-icons/fa";
 import { Link, useNavigate } from "react-router-dom"
+import FormGroup from '@mui/material/FormGroup';
+import Checkbox from '@mui/material/Checkbox';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import DesktopDatePicker from '@mui/lab/DesktopDatePicker';
+import Stack from '@mui/material/Stack';
 
+
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: "flex",
+        flexWrap: "wrap",
+    },
+    floatingLabelFocusStyle: {
+        color: "#525252"
+    },
+    textField: {
+        width: '100%',
+        marginBottom: '25px !important',
+
+    },
+    dateTime: {
+        marginTop: 20,
+        marginBottom: '20px !important'
+    },
+}));
 
 function DrugDetailsModal({ openModal, setOpenModal, cartItem }) {
-
+    const classes = useStyles();
     const [ttotal, setTotal] = useState(0)
+    const [pickup, setpickup] = useState(false)
+    const [user, setUser] = useState()
+    const [prescription, setprescription] = useState()
+    const [selectedDate, setSelectedDate] = useState(new Date());
+    const [deliveryAddress, setDeliveryAddress] = useState('')
 
+    const handleDateChange = (newValue) => {
+        setSelectedDate(newValue);
+    };
     const navigate = useNavigate()
+
+    console.log(selectedDate,deliveryAddress, pickup)
+
+    useEffect(() => {
+        const user = localStorage.getItem("user")
+        setUser(user)
+    })
 
 
     const totalBill = () => {
-        let total =0
-        for(let i = 0;i< cartItem.length; i++){
-            const subtotal = cartItem[i].drug.unitprice * cartItem[i].quantity 
-            total =  total + subtotal
+        let total = 0
+        for (let i = 0; i < cartItem.length; i++) {
+            const subtotal = cartItem[i].drug.unitprice * cartItem[i].quantity
+            total = total + subtotal
         }
         return parseFloat((total).toFixed(2))
     }
+
+   
+
+
+    const handleChange = (event) => {
+        setpickup(event.target.checked);
+    };
 
     return (
         <div className="details-container">
@@ -60,26 +112,85 @@ function DrugDetailsModal({ openModal, setOpenModal, cartItem }) {
                     <hr />
 
 
+                    {
+                        pickup ? null : (
+                            <Grid container spacing={2}
+                            >
+                                <Grid item xs={12}>
+                                    <TextField
+                                        value={deliveryAddress}
+                                        onChange={e => { setDeliveryAddress(e.target.value) }}
+                                        id="outlined-basic"
+                                        label="Delivery address"
+                                        variant="outlined"
+                                        className={classes.textField}
+                                        InputLabelProps={{
+                                            className: classes.floatingLabelFocusStyle,
+                                        }}
+                                    />
+                                </Grid>
+
+
+                                <Grid item xs={12}>
+                                   
+                                    <LocalizationProvider dateAdapter={AdapterDateFns}>
+                                        <Stack spacing={3}>
+                                            <DesktopDatePicker
+                                            label="When should we deliver"
+                                            inputFormat="MM/dd/yyyy"
+                                            value={selectedDate}
+                                            onChange={handleDateChange}
+                                            renderInput={(params) => <TextField {...params} />}
+                                            />
+                                            </Stack>
+                                        </LocalizationProvider>
+
+                                </Grid>
+                            </Grid>
+                        )
+                    }
+
+                    <FormControlLabel control={<Checkbox
+                        checked={pickup}
+                        onChange={handleChange}
+                        inputProps={{ 'aria-label': 'I will collect the order myself' }}
+                    />} label="I will collect the order myself" />
+
+
                     <div class="total_view">
                         <h2>Total:</h2>
                         <p>{totalBill()}</p>
                     </div>
-                   
-                   <div class="cart-checkout" onClick={() => navigate("/OrderReview", {state:{
-                       type: "Pharmacy",
-                       totalCost: totalBill(),
-                       name: "pharmacy",
-                       discount: 0,
-                       item: {
-                        totalCost: totalBill(),
-                        type: "Pharmacy",
-                        discount: 0,
-                        netTotal: totalBill()
-                       }
-                   }})} >
+
+
+
+                    <div class="cart-checkout" onClick={() => navigate('/OrderReview', {
+                        state: {
+                            item: {
+                                netTotal: ['Basic', 'Premium', 'Family'].includes(user.subscription) ? totalBill() : totalBill() + 5,
+                                totalCost: ['Basic', 'Premium', 'Family'].includes(user.subscription) ? totalBill() : totalBill() + 5,
+                                totalDiscount: 0,
+                            },
+                            data: {
+                                order: cartItem,
+                                prescription: 'not required',
+                                deliveryOption: pickup ? 'Pickup' : 'Delivery',
+                                deliveryLocation: {
+                                    address_type: "null",
+                                    contact_no: "null",
+                                    location_address: deliveryAddress,
+                                    name: "null"
+                                },
+                                deliveryTime: selectedDate,
+                                isDirectDoctorPrescription: prescription ? true : false,
+                                serviceId: 'orders'
+                            },
+                            type: 'drug'
+                        }
+                    })} >
                         <p>CHECKOUT</p>
                     </div>
-                   
+
                 </DialogContent>
             </Dialog>
         </div>
