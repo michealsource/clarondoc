025e089a1eb6013e5d103b39ad2d500970969c36
chibuike-vincent <chibuikepatrick2@gmail.com>
2022-02-21 11:52:45 +0100

    COmpleted audio call

--- src/Pages/Call/VideoCall.js
+++ src/Pages/Call/VideoCall.js
@@ -1,33 +1,37 @@
 import React, {useState, useEffect} from 'react'
 import {Controls} from "./Controls"
 import Videos from "./Videos"
+import Audio from "./Audio"
 
 function VideoCall(props) {
-    const { setInCall, channelName, useClient, useMicrophoneAndCameraTracks, token, appId } = props;
+    const { setInCall, channelName, useClient, useMicrophoneAndCameraTracks, token, appId, trackType } = props;
     const [users, setUsers] = useState([]);
     const [start, setStart] = useState(false);
     const client = useClient();
     const { ready, tracks } = useMicrophoneAndCameraTracks();
 
+    
+console.log(tracks,"trackstrackstracks")
     useEffect(() => {
         // function to initialise the SDK
         let init = async (name) => {
           client.on("user-published", async (user, mediaType) => {
             await client.subscribe(user, mediaType);
-            console.log("subscribe success");
+            console.log(user, "subscribe success");
+          
             if (mediaType === "video") {
               setUsers((prevUsers) => {
                 return [...prevUsers, user];
               });
             }
-            if (mediaType === "audio") {
+            if (mediaType === "audio" ) {
               user.audioTrack?.play();
             }
           });
     
           client.on("user-unpublished", (user, type) => {
             console.log("unpublished", user, type);
-            if (type === "audio") {
+            if (type === "audio" ) {
               user.audioTrack?.stop();
             }
             if (type === "video") {
@@ -45,9 +49,15 @@ function VideoCall(props) {
           });
     
           await client.join(appId, name, token, null);
-          if (tracks) await client.publish([tracks[0], tracks[1]]);
-          setStart(true);
-    
+          if (tracks && trackType === "audio") {
+                await tracks[1].close()
+                await client.publish([tracks[0]]);
+                setStart(true);
+            }else if(tracks && trackType !== "audio"){
+                await client.publish([tracks[0], tracks[1]]);
+                setStart(true);
+            }
+            
         };
     
         if (ready && tracks) {
@@ -60,9 +70,10 @@ function VideoCall(props) {
     return (
         <div className="App">
         {ready && tracks && (
-          <Controls tracks={tracks} setStart={setStart} useClient={useClient} setInCall={setInCall} />
+          <Controls tracks={tracks} setStart={setStart} useClient={useClient} setInCall={setInCall} trackType={trackType} />
         )}
-        {start && tracks && <Videos users={users} tracks={tracks} />}
+        {start && tracks && trackType === "audio" ? (<Audio users={users} tracks={tracks} />) : start && tracks && trackType === "video" ? (<Videos users={users} tracks={tracks} />) : null}
+        {/* {start && tracks && <Videos users={users} tracks={tracks} />} */}
       </div>
     )
 }
