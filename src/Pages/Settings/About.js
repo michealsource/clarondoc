d1b85923616fc6a44e603029dbc13e83c05850c8
chibuike-vincent <chibuikepatrick2@gmail.com>
2022-03-08 15:35:57 +0100

    updated the terms

--- src/Pages/Settings/About.js
+++ src/Pages/Settings/About.js
@@ -7,10 +7,8 @@ function About() {
     return (
         <>
         <Navbar/>
-        <div className="about-us-container">
-            <h2>Health is wealth</h2>
-            <p>We combine modern health strategies and technological expertise to create digital experiences that addresses the challenges of accessing medical health.We do this by offering low cost medical services from online to door-step outreach.</p>
-
+       
+           
             <div class="about-us-content-section">
                 <div className="about-text">
                <h4>About Us</h4>
@@ -22,7 +20,7 @@ function About() {
                 </div>
                 
             </div>
-        </div>
+        
         </>
     )
 }
