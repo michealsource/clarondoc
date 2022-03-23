8507fd8b3883170c5d89d05454e5ae1a9f1e757b
Learnitnigeria <learnitnigeria.com>
2022-03-23 13:51:44 +0100

    subscribe

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -10,31 +10,31 @@ import Homecare from '../Pages/Homecare/Homecare'
 import Ambulance from '../Pages/Ambulance/Ambulance'
 import Laboratory from '../Pages/Laboratory/Laboratory'
 // import AppointmentHistory from '../Pages/Appointmenthistory/AppointmentHistory'
-import UserProfile from '../UserDetails/Profile/Profile'
-import Editpatient from '../UserDetails/EditPatient/Editpatient'
-import DemandBooking from '../Component/DemandBookingModal/DemandBooking'
-import IndividualRequest from '../Component/Forms/IndividualRequest'
-import FacilityRequest from '../Component/Forms/FacilityRequest'
-import Drugs from '../Pages/Drugs/Drugs'
-import OtcDrugs from '../Pages/OtcDrugs/OtcDrugs'
-import PrescribedDrugs from '../Pages/OtcDrugs/PrescribedDrugs'
-import Prescribed from '../Pages/Prescribed/Prescribed'
-import OrderReview from '../Pages/OrderReview/OrderReview'
-import HomeCareForm from '../Pages/Homecare/HomeCareForm'
-import Blog from '../Pages/Blog/Blog'
-import PendingOrders from '../Pages/PendingOrders/PendingOrders'
-import SavedDoctors from '../Pages/SavedDoctors/SavedDoctors'
-import Referral from '../Pages/Referral/Referral'
-import Wallet from '../Pages/Wallet/Wallet'
-import Notification from '../Pages/Notification/Notification'
-import Chat from '../Pages/Chat/Chat'
-import AboutClaron from '../Pages/AboutClaron/AboutClaron'
-import ClaronTerms from '../Pages/ClaronTerms/ClaronTerms'
-import SubscriptionSummary from '../Pages/Subscribe/SubscriptionSummary'
-import PayAsYouGo from '../Pages/Subscribe/PayAsYouGo'
-import Book from '../Pages/Book/Book'
-import Dash from '../Pages/Dash/Dash'
-import Call from '../Pages/Call/audioCalls'
+// import UserProfile from '../UserDetails/Profile/Profile'
+// import Editpatient from '../UserDetails/EditPatient/Editpatient'
+// import DemandBooking from '../Component/DemandBookingModal/DemandBooking'
+// import IndividualRequest from '../Component/Forms/IndividualRequest'
+// import FacilityRequest from '../Component/Forms/FacilityRequest'
+    import Drugs from '../Pages/Drugs/Drugs'
+// import OtcDrugs from '../Pages/OtcDrugs/OtcDrugs'
+// import PrescribedDrugs from '../Pages/OtcDrugs/PrescribedDrugs'
+// import Prescribed from '../Pages/Prescribed/Prescribed'
+// import OrderReview from '../Pages/OrderReview/OrderReview'
+// import HomeCareForm from '../Pages/Homecare/HomeCareForm'
+// import Blog from '../Pages/Blog/Blog'
+// import PendingOrders from '../Pages/PendingOrders/PendingOrders'
+// import SavedDoctors from '../Pages/SavedDoctors/SavedDoctors'
+// import Referral from '../Pages/Referral/Referral'
+// import Wallet from '../Pages/Wallet/Wallet'
+// import Notification from '../Pages/Notification/Notification'
+// import Chat from '../Pages/Chat/Chat'
+// import AboutClaron from '../Pages/AboutClaron/AboutClaron'
+// import ClaronTerms from '../Pages/ClaronTerms/ClaronTerms'
+// import SubscriptionSummary from '../Pages/Subscribe/SubscriptionSummary'
+// import PayAsYouGo from '../Pages/Subscribe/PayAsYouGo'
+// import Book from '../Pages/Book/Book'
+// import Dash from '../Pages/Dash/Dash'
+// // import UserProfileDropDown from '../Pages/Dashboard/UserProfileDropDown'
 
 // // DOCTORS PAGE IMPORT
 // import Home from '../DoctorPages/Home/Home'
@@ -53,14 +53,14 @@ import Call from '../Pages/Call/audioCalls'
 // import TermsD from '../DoctorPages/Terms/Terms'
 // import AboutD from '../DoctorPages/About/About'
 // import CartModal from '../DoctorPages/Modals/CartModal'
-
+// import Call from '../Pages/Call/audioCalls'
 // PUBLIC PAGES
 import SignUp from '../Pages/signup/Signup'
 import SignIn from '../Pages/SignIn/SignIn'
 import About from '../Pages/Settings/About'
 import LandingPage from '../Pages/landingPage/LandingPage'
 import TermsandCondition from '../Pages/Settings/TermsandCondition'
-import ChangePassword from '../Pages/ChangePassword/ChangePassword'
+// import ChangePassword from '../Pages/ChangePassword/ChangePassword'
 // import Navbar from '../Component/Navbar/Navbar'
 function index() {
     return (
@@ -72,16 +72,21 @@ function index() {
                 <Route exact path="/homecare" element={<Homecare />} />
                 <Route exact path="/laboratory" element={<Laboratory />} />
                 <Route exact path="/consultation" element={<Consultation />} />
-                {/* <Route exact path="/AppointmentHistory" element={<AppointmentHistory/>} /> */}
-               
+                <Route exact path="/drugs" element={< Drugs />}/>
                 <Route exact path="/Subscribe" element={<Subscribe />} />
+                {/* <Route exact path="/Referral" element={< Referral />} /> */}
+                {/* <Route exact path="/AppointmentHistory" element={<AppointmentHistory/>} /> */}
+                {/* 
+               
+               
+                
                
                 <Route exact path="/profile" element={<UserProfile />} />
                 <Route exact path="/Editpatient" element={<Editpatient />} />
                 <Route exact path="/demandbooking"  element={< DemandBooking />} />
                 <Route exact path="/individualRequest" element={< IndividualRequest />}/>
                 <Route exact path="/facilityrequest" element={< FacilityRequest />} />
-                <Route exact path="/drugs" element={< Drugs />}/>
+                
                 <Route exact path="/otcdrugs" element={< OtcDrugs />} />
                 <Route exact path="/prescribedDrugs" element={< PrescribedDrugs />} />
                 <Route exact path="/prescribed" element={< Prescribed />} />
@@ -90,7 +95,7 @@ function index() {
                 <Route exact path="/blog"  element={< Blog />}/>
                 <Route exact path="/pending" element={< PendingOrders />} />
                 <Route exact path="/SavedDoctors" element={< SavedDoctors />} />
-                <Route exact path="/Referral" element={< Referral />} />
+                
                 <Route exact path="/Wallet" element={< Wallet />} />
                 <Route exact path="/notification" element={< Notification />} />
                 <Route exact path="/chat" element={< Chat />} />
